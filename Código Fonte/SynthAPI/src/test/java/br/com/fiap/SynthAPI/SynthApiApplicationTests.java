c37c9e0d9a9b2bd84680a5587fb1dd7f073a0cd8
RicardoBritoFiap <rbponticceli@gmail.com>
2024-04-10 11:46:19 -0300

    Base da API Spring Boot

---
+++ Código Fonte/SynthAPI/src/test/java/br/com/fiap/SynthAPI/SynthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.SynthAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
