019e04a680fef0fe3c9e449657a0b127c16884c5
RicardoBritoFiap <rbponticceli@gmail.com>
2024-10-31 05:57:41 -0300

    Delete Synthia Redundancy

--- SynthAPI/src/main/java/com/RicardoBritoFiap/SynthAPI/repository/SynthiaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.RicardoBritoFiap.SynthAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.RicardoBritoFiap.SynthAPI.model.Synthia;
-
-public interface SynthiaRepository extends JpaRepository<Synthia, Long> {
-    public Synthia findByCnpjAndEmail(String cnpj, String email);
-}
